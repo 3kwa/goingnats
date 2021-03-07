56274b5c160250ce00acab711d02ecf57d1897ee
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2021-03-07 20:16:55 +1100

    release 2021.3.0 \o/

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+
+from setuptools import setup
+
+curdir = os.path.dirname(__file__)
+readme = os.path.join(curdir, "README.md")
+
+setup(
+    name="goingnats",
+    version="2021.3.0",
+    description="a Python NATS client",
+    long_description=open(readme).read(),
+    long_description_content_type="text/markdown",
+    author="Eugene Van den Bulke",
+    author_email="eugene.vandenbulke@gmail.com",
+    url="http://github.com/3kwa/goingnats",
+    py_modules=["goingnats"],
+    license="Unlicensed",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
